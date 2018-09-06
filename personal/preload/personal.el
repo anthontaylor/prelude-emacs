db45df757433eaa05d3ad2d1f389119f8c943bad
AJ Taylor <anthonjtaylor@gmail.com>
2018-09-05 20:34:55 -0700

    column line width

--- personal/preload/personal.el
+++ personal/preload/personal.el
@@ -1,1 +1,10 @@
-(setq prelude-theme 'tangotango)
+(setq prelude-theme 'adwaita)
+
+(defun my-flymd-browser-function (url)
+  (let ((process-environment (browse-url-process-environment)))
+    (apply 'start-process
+           (concat "firefox " url)
+           nil
+           "/usr/bin/open"
+           (list "-a" "firefox" url))))
+(setq flymd-browser-open-function 'my-flymd-browser-function)
