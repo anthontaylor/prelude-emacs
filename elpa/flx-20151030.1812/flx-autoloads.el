d2c02a8bf9cf9c216c898fbaa8359700bc22055a
AJ Taylor <anthonjtaylor@gmail.com>
2018-09-05 20:38:30 -0700

    add actual packages

---
+++ elpa/flx-20151030.1812/flx-autoloads.el
@@ -0,0 +1,15 @@
+;;; flx-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("flx.el") (23435 25225 970418 530000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; flx-autoloads.el ends here
